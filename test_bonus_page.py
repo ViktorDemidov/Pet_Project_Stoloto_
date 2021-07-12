e8ea26198d3263bc6c78dccc9fd45b2de00cb159
Dmitry Batarev <38316986+dima2308@users.noreply.github.com>
2021-07-12 11:26:33 +0300

    Update test_bonus_page.py

--- test_bonus_page.py
+++ test_bonus_page.py
@@ -15,14 +15,14 @@ def test_check_bonus(browser):
 
 
 @pytest.mark.parametrize('price', [
-    *BonusPageLocators.BONUS_3,
-    *BonusPageLocators.BONUS_9,
-    *BonusPageLocators.BONUS_30,
-    *BonusPageLocators.BONUS_90])
+    BonusPageLocators.BONUS_3,
+    BonusPageLocators.BONUS_9,
+    BonusPageLocators.BONUS_30,
+    BonusPageLocators.BONUS_90])
 def test_buy_bonus_3(browser, price):
     link = "https://tifa.stoloto.ru/"
     page = MainPage(browser, link)
     page.open()
     page.auth_page()
     page = BonusPage(browser, link)
-    page.buy_bonus_3()
+    page.buy_bonus_3(price)
