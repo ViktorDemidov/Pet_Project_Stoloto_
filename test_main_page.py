53e017e99a01ebe6ce1cd4c06a227e07443bec67
V.Demidov <79601348+Richard-Sapagof@users.noreply.github.com>
2021-08-11 15:18:40 +0300

    delete application

--- test_main_page.py
+++ test_main_page.py
@@ -1,26 +1,19 @@
 from page.main_page import MainPage
 import time
-import pytest
-from page.application import Application
 
 
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()#инициализация создание фикстуры
-    request.addfinalizer(fixture.destroy)#указание как должна фикстура быть разрушена
-    return fixture
-
-def test_login_page(app):#тестовый метод принимающий в себя фикстуру
+def test_login_page(browser):
     link = 'https://tifa.stoloto.ru/'
-    app.open()
-    app.auth_page()
+    page = MainPage(browser, link)
+    page.open()
+    page.auth_page()
 
-@pytest.mark.skip
+
 def test_change_nick(browser):#проверка ника
     link = 'https://tifa.stoloto.ru/'
-    app.open()
-    app.auth_page()
-    # time.sleep(5)
-    #page.change_nick()
-    #time.sleep(5)
+    page = MainPage(browser, link)
+    page.open()
+    page.auth_page()
+    time.sleep(5)
+    page.change_nick()
+    time.sleep(5)
